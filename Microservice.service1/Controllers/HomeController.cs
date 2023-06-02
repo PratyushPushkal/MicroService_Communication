9edda02c0155b2a5c53fba43473b579be5103f0b
Pratyush Pushkal <pratyushpushkal@live.com>
2023-06-02 18:00:24 +0530

    Added Nuget Package

--- Microservice.service1/Controllers/HomeController.cs
+++ Microservice.service1/Controllers/HomeController.cs
@@ -1,17 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microservice.Service.Nuget;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Microservice.service1.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class HomeController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
@@ -23,13 +25,14 @@ namespace Microservice.service1.Controllers
             _configuration = configuration;
         }
 
-        [HttpGet]
+        [HttpGet("GetbyhttpClinet")]
         public IActionResult Get()
         {
             HttpClient httpClient = new HttpClient();
             HttpRequestMessage requestMessage = new HttpRequestMessage();
             requestMessage.Method = HttpMethod.Get;
             requestMessage.RequestUri = new Uri(_configuration.GetValue<string>("ServiceUrl")+ "WeatherForecast");
+
             var resp = httpClient.SendAsync(requestMessage);
             resp.Wait();
             if (resp.Result.IsSuccessStatusCode)
@@ -42,6 +45,13 @@ namespace Microservice.service1.Controllers
             {
                 return null;
             }
+        }
+
+        [HttpGet("GetbyNuget")]
+        public IActionResult GetbyNuget()
+        {
+            var result = new WeatherForecastService().GetWeatherForcast();
+            return Ok(result);
         }
     }
 }
